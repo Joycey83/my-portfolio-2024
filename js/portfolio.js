b4075c71c5561bb27e4e6fc7d2ebbcc25ed98f10
Joycey <Jcheung8385@gmail.com>
2023-06-02 12:36:02 +0100

    added the shecodes workshop links and added the project paragraph

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,7 +6,7 @@ var rootElement = document.documentElement;
 function handleScroll() {
   var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
   if (rootElement.scrollTop / scrollTotal > 0.3) {
-    // Show button
+    // Show button when the page is scrolled 30%
     scrollToTopBtn.classList.add("showBtn");
   } else {
     // Hide button
