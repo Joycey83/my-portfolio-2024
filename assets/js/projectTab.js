748d2511a7dc63ac42e77d09fc06c1f94c73cef5
Joyce <Jcheung8385@gmail.com>
2024-03-27 08:32:37 +0000

    added my javascript code to get the tab functionality to work on each of my individual projects

---
+++ assets/js/projectTab.js
@@ -0,0 +1,38 @@
+// Get all articles
+const articles = document.querySelectorAll(".about");
+
+// Loop through each article
+articles.forEach(function (article) {
+  // Get all buttons within the current article
+  var tabBtns = article.querySelectorAll(".tab-btn");
+
+  // Loop through each button
+  tabBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      // Get current button
+      const currentBtn = e.target;
+
+      // Remove active class from all buttons within the current article
+      tabBtns.forEach(function (btn) {
+        btn.classList.remove("active");
+      });
+
+      // Add active class to current button
+      currentBtn.classList.add("active");
+
+      // Get all content within the current article
+      const contents = article.querySelectorAll(".content");
+
+      // Loop through each content
+      contents.forEach(function (content) {
+        // Remove active class from all contents
+        content.classList.remove("active");
+
+        // Add active class to the content that matches the button's data-id
+        if (content.id === currentBtn.dataset.id) {
+          content.classList.add("active");
+        }
+      });
+    });
+  });
+});
